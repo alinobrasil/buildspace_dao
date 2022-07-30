0fb19da94c9545902c3ad818447d78ceac224c58
Ali Kim <alikim@gmail.com>
2022-07-30 16:03:56 -0400

    tell user to switch network if not using rinkeby

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useAddress, useMetamask, useEditionDrop, useToken, useVote, useNetwork 
 import { ChainId } from '@thirdweb-dev/sdk';
 import { useState, useEffect, useMemo } from 'react';
 import { AddressZero } from "@ethersproject/constants";
+import { networks } from './networks';
 
 
 const App = () => {
@@ -16,7 +17,46 @@ const App = () => {
   const token = useToken("0xBE1C7DdFCdD2A520c9a808d9EaCc1c36A993FffD")
   const vote = useVote("0xE552B3a5A99EB3fB5E86C0FCE378Fd8a82634fb5");
 
-
+  const switchNetwork = async () => {
+    if (window.ethereum) {
+      try {
+        // Try to switch to the Mumbai testnet
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x4' }], // Check networks.js for hexadecimal network ids
+        });
+      } catch (error) {
+        // This error code means that the chain we want has not been added to MetaMask
+        // In this case we ask the user to add it to their MetaMask
+        if (error.code === 4902) {
+          try {
+            await window.ethereum.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainId: '0x4',
+                  chainName: 'Rinkeby',
+                  rpcUrls: ['https://rinkeby.infura.io/v3/'],
+                  nativeCurrency: {
+                    name: "rinkeby ETH",
+                    symbol: "rinETH",
+                    decimals: 18
+                  },
+                  blockExplorerUrls: ["https://rinkeby.etherscan.io"]
+                },
+              ],
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        console.log(error);
+      }
+    } else {
+      // If window.ethereum is not found then MetaMask is not installed
+      alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html');
+    }
+  }
 
   // State variable for us to know if user has our NFT.
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
@@ -187,9 +227,15 @@ const App = () => {
         <h2>Please connect to Rinkeby</h2>
         <p>
           This dapp only works on the Rinkeby network, please switch networks
-          in your connected wallet.
+          in your connected wallet. asdfasd
         </p>
+
+        <div className="connect-wallet-container">
+          <p>Please connect to the Rinkeby Network</p>
+          <button className='cta-button mint-button' onClick={switchNetwork}>Click here to switch</button>
+        </div>
       </div>
+
     );
   }
 
